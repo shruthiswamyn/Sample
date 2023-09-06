aaad54328141441af4aea25edc6b18b4ecf925b6
ckvin <ckvin@CKV-Laptop>
2023-09-06 10:58:13 +0530

    commit 1

---
+++ src/test/java/sample/HiMessageTest.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class HiMessageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi how are you???");
+	}
+
+}
